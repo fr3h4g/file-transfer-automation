22ad0b9fab7fb9dfde4f10e08edff9315ac2a3c1
Fredrik Haglund <haglundfre@gmail.com>
2023-03-28 19:03:34 +0200

    log if no files found

--- filetransferautomation/transfer/protocols/base_protocol.py
+++ filetransferautomation/transfer/protocols/base_protocol.py
@@ -94,6 +94,8 @@ class BaseProtocol:
                 )
 
                 done_files = upload_files_out
+        else:
+            logging.info(f"No files found to transfer.")
 
         self._disconnect()
         logging.info(f"Disconnected from '{self._task.name}'.")
