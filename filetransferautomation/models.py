2fc5e76aee94bd9942664cf24ba39d3dbfa11d54
Fredrik Haglund <haglundfre@gmail.com>
2023-03-30 14:52:35 +0200

    add process

--- filetransferautomation/models.py
+++ filetransferautomation/models.py
@@ -51,27 +51,43 @@ class Step(Base):
     step_type: Mapped[
         Literal["source"] | Literal["process"] | Literal["destination"]
     ] = mapped_column(String)
-    host_id: Mapped[int | None] = mapped_column(
-        Integer, ForeignKey("hosts.host_id"), default=None
-    )
-    directory: Mapped[str | None] = mapped_column(String, default=None)
-    type: Mapped[
-        Literal["local_directory"] | Literal["rename"] | Literal["unc_share"] | None
-    ] = mapped_column(String, default=None)
+
+    # directory: Mapped[str | None] = mapped_column(String, default=None)
+    # type: Mapped[
+    #     Literal["local_directory"] | Literal["rename"] | Literal["unc_share"] | None
+    # ] = mapped_column(String, default=None)
     file_mask: Mapped[str | None] = mapped_column(String, default=None)
     filename: Mapped[str | None] = mapped_column(String, default=None)
-    username: Mapped[str | None] = mapped_column(String, default=None)
-    password: Mapped[str | None] = mapped_column(String, default=None)
-    share: Mapped[str | None] = mapped_column(String, default=None)
+    # username: Mapped[str | None] = mapped_column(String, default=None)
+    # password: Mapped[str | None] = mapped_column(String, default=None)
+    # share: Mapped[str | None] = mapped_column(String, default=None)
     run_per_file: Mapped[bool | None] = mapped_column(Boolean, default=None)
     name: Mapped[str | None] = mapped_column(String, default=None)
     max_file_count: Mapped[int | None] = mapped_column(Integer, default=None)
 
+    host_id: Mapped[int | None] = mapped_column(
+        Integer, ForeignKey("hosts.host_id"), default=None
+    )
     host: Mapped[Host] = relationship(lazy=False)
+
+    process_id: Mapped[int | None] = mapped_column(
+        Integer, ForeignKey("processes.process_id"), default=None
+    )
+    process: Mapped[Process | None] = relationship()
 
     def __repr__(self):
         """Table repr."""
         return f"Step({self.step_id}, ...)"
+
+
+class Process(Base):
+    """Table processes model."""
+
+    __tablename__ = "processes"
+
+    process_id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+
+    name: Mapped[str | None] = mapped_column(String, default=None)
 
 
 class Host(Base):
---
+++ populate_test_data.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from filetransferautomation import models
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    # echo=True,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+db = SessionLocal()
+
+sql = []
+sql.append(
+    models.Host(
+        host_id=1,
+        name="test local input",
+        type="local_directory",
+        directory="./test-input",
+    )
+)
+sql.append(
+    models.Host(
+        host_id=2,
+        name="test local output",
+        type="local_directory",
+        directory="./test-output",
+    )
+)
+sql.append(
+    models.Schedule(
+        schedule_id=1,
+        task_id=1,
+        cron="*/1 * * * *",
+    )
+)
+sql.append(
+    models.Step(step_id=1, task_id=1, host_id=1, file_mask="*.txt", step_type="source")
+)
+sql.append(
+    models.Step(
+        step_id=2, task_id=1, host_id=2, file_mask="*.txt", step_type="destination"
+    )
+)
+sql.append(models.Task(task_id=1, name="test", description="", active=1))
+db.add_all(sql)
+db.commit()
