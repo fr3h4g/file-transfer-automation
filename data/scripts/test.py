40f0b2598f03fcaed4faa1ee85051d3f5c125001
Fredrik Haglund <haglundfre@gmail.com>
2023-04-01 21:16:14 +0200

    add sleep in test script

--- data/scripts/test.py
+++ data/scripts/test.py
@@ -2,7 +2,14 @@
 # ruff: noqa: T201
 import os
 from pathlib import Path
+from time import sleep
 
 print(Path(os.path.curdir).resolve())
 
 print("this is from test.py")
+
+print("sleeping...")
+
+sleep(10)
+
+print("waked up")
