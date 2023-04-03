c11d09e8a7bf4db8d7e80a868ecb4ded9b7f8c48
Fredrik Haglund <haglundfre@gmail.com>
2023-04-03 11:46:18 +0200

    fix database url

--- filetransferautomation/database.py
+++ filetransferautomation/database.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
+from filetransferautomation import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
--- filetransferautomation/settings.py
+++ filetransferautomation/settings.py
@@ -5,21 +5,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-FOLDERS_DIR = "./data/folders"
-WORK_DIR = "./data/work"
-SCRIPTS_DIR = "./data/scripts"
-
-MYSQL_HOST: str = str(os.getenv("MYSQL_HOST"))
-MYSQL_USER: str = str(os.getenv("MYSQL_USER"))
-MYSQL_PASS: str = str(os.getenv("MYSQL_PASS"))
-MYSQL_DB: str = str(os.getenv("MYSQL_DB"))
-
 DEV_MODE: bool = bool(os.getenv("DEV_MODE", False))  # used for local development
 
 DIR_ADDON = ""
 if DEV_MODE:
     DIR_ADDON = "."
+    DATABASE_URL = "sqlite:///test.db"
 
-FOLDERS_DIR = DIR_ADDON + "/data/folders"
-WORK_DIR = DIR_ADDON + "/data/work"
-SCRIPTS_DIR = DIR_ADDON + "/data/scripts"
+DATA_DIR = "/data"
+FOLDERS_DIR = DIR_ADDON + os.path.join(DATA_DIR, "folders")
+WORK_DIR = DIR_ADDON + os.path.join(DATA_DIR, "work")
+SCRIPTS_DIR = DIR_ADDON + os.path.join(DATA_DIR, "scripts")
+SCRIPTS_DIR = DIR_ADDON + os.path.join(DATA_DIR, "scripts")
+
+DATABASE_URL = f"sqlite:///{DATA_DIR}/file-transfer-automation.db"
+
+if DEV_MODE:
+    DATABASE_URL = "sqlite:///test.db"
