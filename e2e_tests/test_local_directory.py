a222539c748598878ccfa59a33a0b618cc0994b0
Fredrik Haglund <fr3h4g@gmail.com>
2023-04-15 14:53:22 +0200

    add e2e test local directory

---
+++ e2e_tests/test_local_directory.py
@@ -0,0 +1,49 @@
+"""Test against FTP docker image."""
+from filetransferautomation import models
+from filetransferautomation.models import Host
+from filetransferautomation.database import engine
+from filetransferautomation.step_plugins.local_directory import (
+    DownloadFiles,
+    UploadFiles,
+)
+
+
+def test_db_setup():
+    """Setup db."""
+    models.Base.metadata.create_all(bind=engine)
+
+
+def test_local_directory_upload():
+    """local_directory upload test."""
+    host = Host(
+        directory="./test-output",
+    )
+    download = UploadFiles(
+        arguments='{"file_filter":"test.txt", "delete_files":false}',
+        variables={
+            "host": host,
+            "workspace_directory": ".",
+            "workspace_id": "test",
+            "step_id": 1,
+            "task_id": 1,
+        },
+    )
+    download.process()
+
+
+def test_local_directory_download():
+    """local_directory download test."""
+    host = Host(
+        directory="./test-input",
+    )
+    download = DownloadFiles(
+        arguments='{"file_filter":"test.txt", "delete_files":false}',
+        variables={
+            "host": host,
+            "workspace_directory": ".",
+            "workspace_id": "test",
+            "step_id": 1,
+            "task_id": 1,
+        },
+    )
+    download.process()
