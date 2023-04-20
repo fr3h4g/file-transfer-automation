9e90f9dc25f2358689aa1ca16d719b6afa68300e
Fredrik Haglund <fr3h4g@gmail.com>
2023-04-20 09:04:04 +0200

    add description + output model

--- filetransferautomation/plugins.py
+++ filetransferautomation/plugins.py
@@ -15,6 +15,11 @@ def get_plugins():
     step_plugins = PluginCollection("filetransferautomation.step_plugins")
     for plugin in step_plugins.plugins:
         plugins.append(
-            {"name": plugin.name, "input_model": plugin.input_model.schema()}
+            {
+                "name": plugin.name,
+                "description": plugin.__doc__,
+                "input_model": plugin.input_model.schema(),
+                "output_model": plugin.output_model.schema(),
+            }
         )
     return plugins
