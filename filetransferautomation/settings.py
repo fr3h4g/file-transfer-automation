0adebb485309c9b89c3ccc84f02d7087404bc90c
Fredrik Haglund <fr3h4g@gmail.com>
2023-05-24 13:17:19 +0200

    add email step

--- filetransferautomation/settings.py
+++ filetransferautomation/settings.py
@@ -24,3 +24,9 @@ if DEV_MODE:
 DATABASE_URL = str(os.getenv("DATABASE_URL", DATABASE_URL))
 
 DISABLE_JOBS: bool = bool(os.getenv("DISABLE_JOBS", False))
+
+SMTP_HOSTNAME = str(os.getenv("SMTP_HOSTNAME", "localhost"))
+SMTP_USERNAME = str(os.getenv("SMTP_USERNAME", ""))
+SMTP_PASSWORD = str(os.getenv("SMTP_PASSWORD", ""))
+SMTP_PORT: int = int(os.getenv("SMTP_PORT", 25))
+SMTP_TLS: bool = bool(os.getenv("SMTP_TLS", False))
